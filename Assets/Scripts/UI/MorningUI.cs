e49fc734d5e283224d398e5d8fbcba220c156ec9
seunghee-2002 <dldudcjs12345@gmail.com>
2025-05-27 15:43:14 +0900

    Revert "MVP"
    This reverts commit 8bcf9818fc569792c9af96a44d0013e20a63221f.

--- Assets/Scripts/UI/MorningUI.cs
+++ Assets/Scripts/UI/MorningUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MorningUI : MonoBehaviour
 {
@@ -95,7 +96,8 @@ public class MorningUI : MonoBehaviour
             RecipeData recipe = BlacksmithManager.Instance.recipes[i];
 
             GameObject btn = Instantiate(forgeButtonPrefab, forgeButtonParent);
-            btn.GetComponentInChildren<TextMeshProUGUI>().text = $"{recipe.resultWeapon.weaponName} ({recipe.cost}G)";
+            TextMeshProUGUI txt = btn.GetComponentInChildren<TextMeshProUGUI>();
+            txt.text = $"{recipe.resultWeapon.weaponName} ({recipe.cost}G)";
 
             btn.GetComponent<Button>()
                .onClick.AddListener(() => {
@@ -106,10 +108,10 @@ public class MorningUI : MonoBehaviour
 
     void ShowBlacksmithForgePanel()
     {
+        panelController.ShowBlacksmithForgePanel();
+
         // 레시피 UI 업데이트
         UpdateBlacksmithUI();
-
-        panelController.ShowBlacksmithForgePanel();
     }
 
     void ShowBlacksmithRecipePanel()
